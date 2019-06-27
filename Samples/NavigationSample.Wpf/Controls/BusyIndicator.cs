fa4b9de11d7d9de4d14a2d318192aa22f03c119e
romagny13 <romagny13@yahoo.fr>
2019-06-27 20:28:41 +0200

    Navigation sample

--- Samples/NavigationSample.Wpf/Controls/BusyIndicator.cs
+++ Samples/NavigationSample.Wpf/Controls/BusyIndicator.cs
@@ -1,21 +1,27 @@
-﻿using System;
+﻿using MvvmLib.Utils;
+using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
 using System.Windows.Threading;
 
 namespace NavigationSample.Wpf.Controls
 {
     public class BusyIndicator : Control
     {
+        private DispatcherTimer displayAfterTimer;
+
         static BusyIndicator()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(BusyIndicator), new FrameworkPropertyMetadata(typeof(BusyIndicator)));
         }
 
-
         public BusyIndicator()
         {
             this.Visibility = Visibility.Collapsed;
+            this.displayAfterTimer = new DispatcherTimer();
+            this.displayAfterTimer.Tick += OnDisplayAfterTimerTick;
         }
 
         public string Message
@@ -42,23 +48,6 @@ namespace NavigationSample.Wpf.Controls
             busyIndicator.SetIsBusy((bool)e.NewValue);
         }
 
-        private void SetIsBusy(bool isBusy)
-        {
-            var timer = new DispatcherTimer { Interval = DisplayAfter };
-            EventHandler timerCallback = null;
-            timerCallback = (s, e) =>
-            {
-                timer.Stop();
-                timer.Tick -= timerCallback;
-                if (isBusy)
-                    this.Visibility = Visibility.Visible;
-                else
-                    this.Visibility = Visibility.Collapsed;
-            };
-            timer.Tick += timerCallback;
-            timer.Start();
-        }
-
         public TimeSpan DisplayAfter
         {
             get { return (TimeSpan)GetValue(DisplayAfterProperty); }
@@ -66,7 +55,82 @@ namespace NavigationSample.Wpf.Controls
         }
 
         public static readonly DependencyProperty DisplayAfterProperty =
-            DependencyProperty.Register("DisplayAfter", typeof(TimeSpan), typeof(BusyIndicator), new PropertyMetadata(new TimeSpan(0,0,0,0,100)));
+            DependencyProperty.Register("DisplayAfter", typeof(TimeSpan), typeof(BusyIndicator), new PropertyMetadata(new TimeSpan(0, 0, 0, 0, 100)));
 
+        public string FocusAfterBusy
+        {
+            get { return (string)GetValue(FocusAfterBusyProperty); }
+            set { SetValue(FocusAfterBusyProperty, value); }
+        }
+
+        public static readonly DependencyProperty FocusAfterBusyProperty =
+            DependencyProperty.Register("FocusAfterBusy", typeof(string), typeof(BusyIndicator), new PropertyMetadata(null));
+
+        private void SetIsBusy(bool isBusy)
+        {
+            if (isBusy)
+            {
+                if (DisplayAfter.Equals(TimeSpan.Zero))
+                    this.Visibility = Visibility.Visible;
+                else
+                {
+                    this.displayAfterTimer.Interval = DisplayAfter;
+                    this.displayAfterTimer.Start();
+                }
+            }
+            else
+            {
+                this.displayAfterTimer.Stop();
+                this.Visibility = Visibility.Collapsed;
+
+                if (FocusAfterBusy != null)
+                    FocusHelper.Focus(FocusAfterBusy, this.Parent);
+            }
+        }
+
+        private void OnDisplayAfterTimerTick(object sender, EventArgs e)
+        {
+            this.displayAfterTimer.Stop();
+            this.Visibility = Visibility.Visible;
+        }
+
+       
+    }
+
+    public class FocusHelper
+    {
+        public static void Focus(string elementName, DependencyObject parent)
+        {
+            if (elementName == null)
+                throw new ArgumentNullException(nameof(elementName));
+            if (parent == null)
+                throw new ArgumentNullException(nameof(parent));
+
+            var element = FindElementByName(elementName, parent);
+            if (element != null)
+                element.Dispatcher.BeginInvoke(DispatcherPriority.Input, new Action(() => element.Focus()));
+        }
+
+        private static FrameworkElement FindElementByName(string elementName, DependencyObject parent)
+        {
+            int childrenCount = VisualTreeHelper.GetChildrenCount(parent);
+            for (int i = 0; i < childrenCount; i++)
+            {
+                var child = VisualTreeHelper.GetChild(parent, i);
+                if (child != null)
+                {
+                    if (child is FrameworkElement element)
+                    {
+                        if (element.Name == elementName)
+                            return element;
+                    }
+
+                    var childElement = FindElementByName(elementName, child);
+                    if (childElement != null)
+                        return childElement;
+                }
+            }
+            return null;
+        }
     }
 }
--- Samples/NavigationSample.Wpf/Views/ViewH.xaml.cs
+++ Samples/NavigationSample.Wpf/Views/ViewH.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using MvvmLib.Message;
 using NavigationSample.Wpf.ViewModels;
 using System;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace NavigationSample.Wpf.Views
